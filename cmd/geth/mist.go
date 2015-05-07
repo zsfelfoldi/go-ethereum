ae03793423c5ff27fdf51c9321de727babe38524
Bas van Kervel <basvankervel@ziggo.nl>
2015-05-07 10:31:58 +0200

    added mist/geth process handling

---
+++ cmd/geth/mist.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "os"
+    "syscall"
+    "time"
+)
+
+// Returns when mist is stopped (or crashed)
+func waitForMist() {
+    for {
+        timeout := time.After(1 * time.Second)
+        select {
+        case <-timeout:
+            proc, err := os.FindProcess(os.Getppid())
+            if err != nil {
+                return
+            }
+
+            err = proc.Signal(syscall.Signal(0))
+            if err != nil {
+                return
+            }
+        }
+    }
+}
