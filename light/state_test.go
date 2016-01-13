280de0f8086085efdf969953ee18b6b8f1696644
zsfelfoldi <zsfelfoldi@gmail.com>
2016-01-13 19:35:48 +0100

    light: implemented light chain, VM env and tx pool

--- light/state_test.go
+++ light/state_test.go
@@ -22,32 +22,13 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/trie"
 	"golang.org/x/net/context"
 )
-
-type testOdr struct {
-	OdrBackend
-	sdb, ldb ethdb.Database
-}
-
-func (odr *testOdr) Database() ethdb.Database {
-	return odr.ldb
-}
-
-func (odr *testOdr) Retrieve(ctx context.Context, req OdrRequest) error {
-	switch req := req.(type) {
-	case *TrieRequest:
-		t, _ := trie.New(req.root, odr.sdb)
-		req.proof = t.Prove(req.key)
-	case *NodeDataRequest:
-		req.data, _ = odr.sdb.Get(req.hash[:])
-	}
-	req.StoreResult(odr.ldb)
-	return nil
-}
 
 func makeTestState() (common.Hash, ethdb.Database) {
 	sdb, _ := ethdb.NewMemDatabase()
@@ -67,9 +48,11 @@ func makeTestState() (common.Hash, ethdb.Database) {
 
 func TestLightStateOdr(t *testing.T) {
 	root, sdb := makeTestState()
+	header := &types.Header{Root: root}
+	core.WriteHeader(sdb, header)
 	ldb, _ := ethdb.NewMemDatabase()
 	odr := &testOdr{sdb: sdb, ldb: ldb}
-	ls := NewLightState(root, odr)
+	ls := NewLightState(StateTrieID(header), odr)
 	ctx := context.Background()
 
 	for i := byte(0); i < 100; i++ {
@@ -151,9 +134,11 @@ func TestLightStateOdr(t *testing.T) {
 
 func TestLightStateSetCopy(t *testing.T) {
 	root, sdb := makeTestState()
+	header := &types.Header{Root: root}
+	core.WriteHeader(sdb, header)
 	ldb, _ := ethdb.NewMemDatabase()
 	odr := &testOdr{sdb: sdb, ldb: ldb}
-	ls := NewLightState(root, odr)
+	ls := NewLightState(StateTrieID(header), odr)
 	ctx := context.Background()
 
 	for i := byte(0); i < 100; i++ {
@@ -227,9 +212,11 @@ func TestLightStateSetCopy(t *testing.T) {
 
 func TestLightStateDelete(t *testing.T) {
 	root, sdb := makeTestState()
+	header := &types.Header{Root: root}
+	core.WriteHeader(sdb, header)
 	ldb, _ := ethdb.NewMemDatabase()
 	odr := &testOdr{sdb: sdb, ldb: ldb}
-	ls := NewLightState(root, odr)
+	ls := NewLightState(StateTrieID(header), odr)
 	ctx := context.Background()
 
 	addr := common.Address{42}
