9a37aa4be239c802383e3df229fc1012430d0748
zsfelfoldi <zsfelfoldi@gmail.com>
2016-06-15 13:47:52 +0200

    1

---
+++ p2p/discover/topic_test.go
@@ -0,0 +1,63 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package discover
+
+import (
+	"encoding/binary"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestTopicRadius(t *testing.T) {
+	topic := Topic("qwerty")
+	rad := newTopicRadius(topic)
+	targetRad := (^uint64(0)) / 100
+	minRad := (^uint64(0)) / 10000
+	
+	waitFn := func(addr common.Hash) time.Duration {
+		prefix := binary.BigEndian.Uint64(addr[0:8])
+		dist := prefix ^ rad.topicHashPrefix
+		relDist := float64(dist) / float64(targetRad)
+		relTime := 1/(relDist+0.5)
+		return time.Duration(float64(targetWaitTime)*relTime)
+	}
+	
+	bcnt := 0
+	cnt := 0
+	var sum float64
+	for cnt < 1000000 {
+		addr := rad.nextTarget()
+		wait := waitFn(addr)
+		ticket := &ticket{
+			topics: []Topic{topic},
+			regTime: []absTime{absTime(wait)},
+		}
+		rad.adjust(absTime(0), ticketRef{ticket, 0}, minRad)
+		if rad.converged {
+			cnt++
+			sum += float64(rad.radius)
+		} else {
+			bcnt++
+		}
+	}
+	avgRel := sum/float64(cnt)/float64(targetRad)
+	fmt.Println(bcnt, cnt, avgRel)
+}
+
