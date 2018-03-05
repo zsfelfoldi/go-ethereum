7542116ebf4d572eb971da75067745803044a954
Zsolt Felfoldi <zsfelfoldi@gmail.com>
2018-03-05 22:23:04 +0100

    111

--- trie/trie.go
+++ trie/trie.go
@@ -57,7 +57,7 @@ func CacheUnloads() int64 {
 // LeafCallback is a callback type invoked when a trie operation reaches a leaf
 // node. It's used by state sync and commit to allow handling external references
 // between account and storage tries.
-type LeafCallback func(leaf []byte, parent common.Hash) error
+type LeafCallback func(leaf []byte, parent common.Hash, path []byte) error
 
 // Trie is a Merkle Patricia Trie.
 // The zero value is an empty trie with no database.
