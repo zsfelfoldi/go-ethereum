61b5f447b9da09aede9f4fab6d83aa5e7429efe6
Péter Szilágyi <peterke@gmail.com>
2018-11-12 18:47:34 +0200

    cmd, core, eth, light, trie: add trie read caching layer

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -134,6 +134,7 @@ var AppHelpFlagGroups = []flagGroup{
 		Flags: []cli.Flag{
 			utils.CacheFlag,
 			utils.CacheDatabaseFlag,
+			utils.CacheTrieFlag,
 			utils.CacheGCFlag,
 			utils.TrieCacheGenFlag,
 		},
