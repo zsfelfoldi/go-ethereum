d7d4f9ae196772cf6466181b5788f5d761bd446c
zsfelfoldi <zsfelfoldi@gmail.com>
2026-04-18 11:18:03 +0200

    333

--- core/logindex/table_set.go
+++ core/logindex/table_set.go
@@ -15,3 +15,129 @@
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 package logindex
+
+type tableSet []rangeSet
+
+type Params struct {
+	tableLevels    []tableLevel
+	protocolLevels []protocolLevel
+}
+
+type tableLevel struct {
+	//TODO storage mode
+	blockCount uint64
+}
+
+type protocolLevel struct {
+	tailAge, headAge uint64
+}
+
+const (
+	opNone = iota
+	opMerge
+	opDelete
+)
+
+type tableOperation struct {
+	operation, tableLevel int
+	tableIndex            uint64
+}
+
+func (p *Params) compareOps(a, b tableOperation) int {
+	switch {
+	case a.operation > b.operation:
+		return 1
+	case a.operation < b.operation:
+		return -1
+	}
+	aa := a.tableIndex * p.tableLevels[a.tableLevel]
+	bb := b.tableIndex * p.tableLevels[b.tableLevel]
+	switch {
+	case aa > bb:
+		return 1
+	case aa < bb:
+		return -1
+	default:
+		return 0
+	}
+}
+
+func (p *Params) nextAction(avail, target tableSet) (tableOperation, common.Range[uint64]) {
+	var (
+		bestOp    tableOperation
+		reqBlocks common.Range[uint64]
+		required  rangeSet
+	)
+	for i := len(p.tableLevels) - 1; i >= 0; i-- {
+		required = required.or(target[i])
+		if remove := avail[i].andNot(required); !remove.isEmpty {
+			op := tableOperation{
+				operation:  opDelete,
+				tableLevel: i,
+				tableIndex: remove.last(),
+			}
+			if p.compareOps(op, bestOp) > 0 {
+				bestOp = op
+			}
+		}
+		required = required.andNot(avail[i])
+		if i > 0 {
+			merge := required.and(shiftTableLevel(avail[i-1], p.tableLevels[i-1], p.tableLevels[i], false))
+			if !merge.isEmpty {
+				op := tableOperation{
+					operation:  opMerge,
+					tableLevel: i,
+					tableIndex: merge.last(),
+				}
+				if p.compareOps(op, bestOp) > 0 {
+					bestOp = op
+				}
+			}
+			required = shiftTableLevel(required.andNot(merge), p.tableLevels[i], p.tableLevels[i-1], false)
+		} else {
+			if !required.isEmpty() {
+				reqBlocks = required.lastSection()
+			}
+		}
+	}
+	return bestOp, reqBlocks
+}
+
+func shiftTableLevel(rs rangeSet, from, to tableLevel, partial bool) rangeSet {
+	for i, r := range rs {
+		first := r.First()*from.blockCount + from.offset - to.offset
+		if !partial {
+			first += to.blockCount - 1
+		}
+		first /= to.blockCount
+		afterLast := r.AfterLast()*from.blockCount + from.offset - to.offset
+		if partial {
+			afterLast += to.blockCount - 1
+		}
+		afterLast /= to.blockCount
+		rs[i] = common.NewRange[uint64](first, afterLast-first)
+	}
+	rs.normalize()
+	return rs
+}
+
+func (ix *Indexer) makeTargetSet() tableSet {
+	target := ix.tableLevels.rangeTarget(ix.valid, rangeSet{common.NewRange[uint64](ix.tailBlock, ix.headBlock+1-ix.tailBlock)})
+	for i, pl := range protocolLevels {
+		first := max(ix.headBlock, pl.tailAge) - pl.tailAge
+		afterLast := max(ix.headBlock+1, pl.headAge) - pl.headAge
+		target[i] = target[i].or(rangeSet{common.NewRange[uint64](first, afterLast-first)})
+	}
+	return target
+}
+
+func (p *Params) rangeTarget(avail tableSet, blockRange rangeSet) tableSet {
+	target := p.tableLevels.newTableSet()
+	for i := len(p.tableLevels) - 1; i >= 0; i-- {
+		fullTables := shiftTableLevel(blockRange, p.tableLevels[0], p.tableLevels[i], false)
+		partialTables := shiftTableLevel(blockRange, p.tableLevels[0], p.tableLevels[i], true)
+		target[i] = fullTables.or(partialTables.and(avail[i]))
+		blockRange = blockRange.andNot(shiftTableLevel(target[i], p.tableLevels[i], p.tableLevels[0], false))
+	}
+	return target
+}
