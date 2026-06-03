4dba9f034054911a64087f643018695537b6c3f4
zsfelfoldi <zsfelfoldi@gmail.com>
2026-06-04 01:14:24 +0200

    qqq

--- core/logindex/prover.go
+++ core/logindex/prover.go
@@ -16,33 +16,151 @@
 
 package logindex
 
+import (
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common/lru"
+)
+
+const (
+	nodeChunkSize      = 1024
+	maxOutputCount     = 4
+	logicAndGate       = uint64(0xffffffff00000000)
+	logicOrGate        = uint64(0xfffffffe00000000)
+	logicGateMask      = uint64(0xffffffff00000000)
+	logicInputMask     = uint64(0x00000000ffffffff)
+	logicGateThreshold = uint64(0xfffffffe00000000)
+)
+
 type tableProver struct {
+	lock       sync.Mutex
+	nodeChunks []*logicNodeChunk
 }
 
 func newTableProver() *tableProver {
-	panic("xxx")
 	return &tableProver{}
 }
 
 func (tp *tableProver) newInstance() *proverInstance {
-	panic("xxx")
-	return &proverInstance{}
+	return &proverInstance{
+		prover: tp,
+		cache:  lru.NewCache[uint32, *logicNodeChunk](10),
+	}
 }
 
-type proverInstance struct{}
+func (tp *tableProver) newChunk() *logicNodeChunk {
+	tp.lock.Lock()
+	defer tp.lock.Unlock()
+
+	chunkIndex := uint32(len(tp.nodeChunks))
+	tp.nodeChunks = append(tp.nodeChunks, &logicNodeChunk{
+		index: chunkIndex,
+	})
+	return tp.nodeChunks[chunkIndex]
+}
+
+func (tp *tableProver) getChunk(index uint32) *logicNodeChunk {
+	tp.lock.Lock()
+	defer tp.lock.Unlock()
+
+	return tp.nodeChunks[index]
+}
+
+/*func (tp *tableProver) getNode(node uint32) *logicNode {
+	if node == 0 {
+		return nil
+	}
+	return &tp.nodeChunks[(node-1)/nodeChunkSize].nodes[(node-1)%nodeChunkSize]
+}*/
+
+type proverInstance struct {
+	prover       *tableProver
+	cache        *lru.Cache[uint32, *logicNodeChunk]
+	currentChunk *logicNodeChunk
+}
+
+func (pi *proverInstance) getNode(node uint32) *logicNode {
+	if node == 0 {
+		return nil
+	}
+	chunkIndex := (node - 1) / nodeChunkSize
+	chunk, ok := pi.cache.Get(chunkIndex)
+	if !ok {
+		chunk = pi.prover.getChunk(chunkIndex)
+		pi.cache.Add(chunkIndex, chunk)
+	}
+	return &chunk.nodes[(node-1)%nodeChunkSize]
+}
 
 func (pi *proverInstance) addLeafNode(entryIndex uint64) uint32 {
-	panic("xxx")
+	if entryIndex >= logicGateThreshold {
+		panic("invalid entry index")
+	}
+	return pi.addNode(entryIndex)
 }
 
 func (pi *proverInstance) addAndNode() uint32 {
-	panic("xxx")
+	return pi.addNode(logicAndGate)
 }
 
 func (pi *proverInstance) addOrNode() uint32 {
-	panic("xxx")
+	return pi.addNode(logicOrGate)
+}
+
+func (pi *proverInstance) addNode(entryOrGate uint64) uint32 {
+	if pi.currentChunk == nil {
+		pi.currentChunk = pi.prover.newChunk()
+		pi.cache.Add(pi.currentChunk.index, pi.currentChunk)
+	}
+	node := pi.currentChunk.index*nodeChunkSize + 1 + uint32(pi.currentChunk.count)
+	pi.currentChunk.nodes[pi.currentChunk.count].entryOrGate = entryOrGate
+	pi.currentChunk.count++
+	if pi.currentChunk.count == nodeChunkSize {
+		pi.currentChunk = nil
+	}
+	return node
 }
 
 func (pi *proverInstance) connect(source, target uint32) {
-	panic("xxx")
+	s := pi.getNode(source)
+	if oc := s.outputCount(); oc < maxOutputCount {
+		s.output[oc] = target
+	} else {
+		split := pi.addOrNode()
+		ss := pi.getNode(split)
+		ss.output = s.output
+		s.output[0] = split
+		s.output[1] = target
+		for i := 2; i < maxOutputCount; i++ {
+			s.output[i] = 0
+		}
+	}
+	t := pi.getNode(target)
+	if !t.isGate() {
+		panic("logic connection target is not a gate node")
+	}
+	t.entryOrGate++
+}
+
+type logicNodeChunk struct {
+	nodes        [nodeChunkSize]logicNode
+	index, count uint32
+}
+
+type logicNode struct {
+	entryOrGate uint64
+	output      [maxOutputCount]uint32
+}
+
+func (ln *logicNode) isGate() bool {
+	return ln.entryOrGate >= logicGateThreshold
+}
+
+func (ln *logicNode) outputCount() int {
+	for i, v := range ln.output {
+		if v == 0 {
+			return i
+		}
+	}
+	return maxOutputCount
 }
